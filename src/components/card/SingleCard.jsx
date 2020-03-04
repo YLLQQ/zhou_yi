1cfc1f5189208f04063c960888a02a023b442e2e
EL <ysh1059@163.com>
2020-03-04 23:25:17 +0800

    update gua

--- src/components/card/SingleCard.jsx
+++ src/components/card/SingleCard.jsx
@@ -4,14 +4,43 @@ import './SingleCard.css'
 
 const SingleCard = () => {
     return (
-        <Collapse>
-            <Collapse.Panel header="Panel-1">
-                11111111111
-            </Collapse.Panel>
-            <Collapse.Panel header="Panel-2">
-                22222222222
-            </Collapse.Panel>
-        </Collapse>
+        <div className="container">
+            <div className="row">
+                <Collapse>
+                    <Collapse.Panel header="Panel-1">
+                        11111111111
+                    </Collapse.Panel>
+                    <Collapse.Panel header="Panel-2">
+                        22222222222
+                    </Collapse.Panel>
+                </Collapse>
+            </div>
+
+            <div className="row">
+                <Collapse>
+                    <Collapse.Panel header="Panel-1">
+                        11111111111
+                    </Collapse.Panel>
+                    <Collapse.Panel header="Panel-2">
+                        22222222222
+                    </Collapse.Panel>
+                </Collapse>
+            </div>
+
+            <div className="gGua">
+                <div className="gYin">
+                    <div className="gLeft" />
+                    <div className="gMiddle" />
+                    <div className="gRight" />
+                </div>
+                <div className="gYang" />
+                <div className="gYin">
+                    <div className="gLeft" />
+                    <div className="gMiddle" />
+                    <div className="gRight" />
+                </div>
+            </div>
+        </div>
     );
 }
 
