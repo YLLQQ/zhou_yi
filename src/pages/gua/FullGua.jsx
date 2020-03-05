11f04d01db9e18e03ab43635ed14d782cca59e26
yangguoqing <ysh1059@163.com>
2020-03-05 13:58:43 +0800

    使用useState更新组件

--- src/pages/gua/FullGua.jsx
+++ src/pages/gua/FullGua.jsx
@@ -1,33 +1,24 @@
-import React, { Component } from 'react';
+import React, { useState } from 'react';
 import SingleCard from "../../components/card/SingleCard";
 import axios from 'axios';
 
-class FullGua extends Component {
+const getDataList = (func) => {
+    axios.get('https://www.fastmock.site/mock/152dc15209e2480ae6d8e3f7e23e0059/zhouyi/all')
+        .then(response => {
+            func(response.data);
+        }).catch((err) => console.log(err));
+}
 
-    constructor(props) {
-        super(props)
+const FullGua = () => {
+    const [dataList, setDataList] = useState([])
 
-        this.state = {
-            dataList: []
-        }
-    }
+    getDataList(setDataList)
 
-    componentDidMount() {
-        axios.get('https://www.fastmock.site/mock/152dc15209e2480ae6d8e3f7e23e0059/zhouyi/all')
-            .then(response => {
-                this.setState({
-                    dataList: response.data
-                })
-            }).catch((err) => console.log(err));
-    }
-
-    render() {
-        return (
-            <div>
-                {this.state.dataList.map(v => <SingleCard dataList={v} />)}
-            </div>
-        );
-    }
+    return (
+        <div>
+            {dataList.map(v => <SingleCard dataList={v} key={Math.random() * 10000} />)}
+        </div>
+    );
 }
 
 export default FullGua;
